349a97b2fefba1f297df4f0adcf366553003764c
Mohamed Saied <mohamed.saied.fathallah@gmail.com>
2025-12-21 01:12:19 +0200

    fix: remove gurdes

--- backend/src/courses/courses.controller.ts
+++ backend/src/courses/courses.controller.ts
@@ -1,11 +1,6 @@
-import { Controller, HttpCode, Post, Body, Get, Delete, HttpStatus, Param, UseGuards } from "@nestjs/common";
+import { Controller, HttpCode, Post, Body, Get, Delete, HttpStatus, Param } from "@nestjs/common";
 import { CoursesService } from "./courses.service";
 import { CoursesDto } from "./dto/courses.dto";
-import { AuthGuard } from "src/auth/guards/auth.guard";
-import { RolesGuard } from "src/auth/guards/roles.guard";
-import { Roles } from "src/auth/decorators/roles.decorator";
-import { Role } from "@prisma/client";
-import { GetUser } from "src/auth/decorators/user.decorator";
 
 
 @Controller('courses')
@@ -14,33 +9,24 @@ export class CoursesController {
     @Get()
     @HttpCode(HttpStatus.OK)
     async findAll() {
-        return this.CoursesService.findAll();
-    }
-
-    @Get(':id')
-    @HttpCode(HttpStatus.OK)
-    async findOne(@Param('id') id: string) {
-        return this.CoursesService.findOne(id);
+        return this.CoursesService.findAll({
+            include: {
+                teacher: {
+                    select: {
+                        name: true
+                    }
+                }
+            }
+        });
     }
 
     @Post()
-    @UseGuards(AuthGuard, RolesGuard)
-    @Roles(Role.ADMIN, Role.SUPERVISOR)
     @HttpCode(HttpStatus.CREATED)
     async create(@Body() dto: CoursesDto) {
         return this.CoursesService.create(dto);
     }
 
-    @Post(':id/enroll')
-    @UseGuards(AuthGuard)
-    @HttpCode(HttpStatus.OK)
-    async enroll(@Param('id') id: string, @GetUser('id') userId: string) {
-        return this.CoursesService.enroll(id, userId);
-    }
-
     @Delete(':id')
-    @UseGuards(AuthGuard, RolesGuard)
-    @Roles(Role.ADMIN, Role.SUPERVISOR)
     @HttpCode(HttpStatus.OK)
     async delete(@Param('id') id: string) {
         return {
