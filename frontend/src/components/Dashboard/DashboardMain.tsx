eb3bf00f725144c52dd44682952f15b08e87d8c2
Mohamed Saied <mohamed.saied.fathallah@gmail.com>
2025-12-25 01:35:33 +0200

    test: env var

--- frontend/src/components/Dashboard/DashboardMain.tsx
+++ frontend/src/components/Dashboard/DashboardMain.tsx
@@ -29,6 +29,7 @@ export const DashboardMain = () => {
     const { user } = useAuth()
     const [courses, setCourses] = useState<Course[]>([])
     const baseUrl = import.meta.env.VITE_BASE_URL
+    console.log(baseUrl);
     
     useEffect(() => {
         const fetchCourses = async () => {
